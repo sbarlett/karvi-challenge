074e2f7d6d3bf5caa6f5b71d06cf6334ddaf67ea
sbarlett <santigobarletta96@gmail.com>
2024-12-15 13:38:36 -0300

    Refactor build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-const dir = path.resolve(__dirname, "build");
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: dir,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
